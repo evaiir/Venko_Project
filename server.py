7da296575325f287f4e5938829dcb9e4900d5ae7
Vinicius Evair da Silva <vsilva.evair@gmail.com>
2024-01-23 10:59:55 -0300

    Implemented multithreading so server can listen to more than one client.

--- server.py
+++ server.py
@@ -1,15 +1,12 @@
-import os
 import socket
+import threading
 
 import communication_utils as comm_utils
-import file_utils as f_utils
 import server_utils as sv_utils
 
 dtprint = comm_utils.timestamped_print
 
-HOME_DIR = os.getcwd()
-MAX_CLIENTS = 1
-TIMESTMP_OFFSET = 22
+MAX_CLIENTS = 5
 
 # Server configuration.
 host = str(socket.INADDR_ANY)  # Bind to any interface
@@ -21,93 +18,33 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Bind the socket to a specific address and port.
 server_socket.bind((host, port))
 
-# Listen for incoming connections (currently only one at a time).
+# Listen for incoming connections.
 server_socket.listen(MAX_CLIENTS)
 dtprint(f"Server listening on any interface on port {port}")
 
-# Accept a connection.
-client_socket, client_address = server_socket.accept()
-dtprint(f"Connection from {client_address} accepted.")
+threads = []
 
-# Run the server requests until client exits.
-while True:
-    requests = sv_utils.receive_request(client_socket)
-    request_type, arg1, *_ = requests + [None]
-    dtprint(f"Client {client_address} requested '{request_type[1:]}' command.")
+try:
+    while True:
+        # Accept a connection.
+        client_socket, client_address = server_socket.accept()
 
-    try:
-        if arg1:
-            full_path = os.path.abspath(os.path.join(os.getcwd(), arg1))
-
-            if not full_path.startswith(HOME_DIR):
-                raise PermissionError(f"Error: You don't have permission to access that directory")
-            comm_utils.send_text(client_socket, "ok")
-        else:
-            full_path = HOME_DIR
-            comm_utils.send_text(client_socket, "ok")
-
-        match request_type:
-            # Both listing functions have to check for error as they don't pass through the
-            # validation function.
-            case "/list" | "/ls" | "/tree":
-                try:
-                    listing_function = {
-                        "/ls": f_utils.list_content,
-                        "/list": f_utils.list_content,
-                        "/tree": f_utils.tree_list_content
-                    }
-                    dir_list = listing_function[request_type](full_path)
-
-                    comm_utils.send_text(client_socket, "ok")
-                    comm_utils.send_text(client_socket, dir_list)
-
-                    prompt = f"Listed a directory for client {client_address}"
-                    prompt += " as a tree." if request_type == "/tree" else "."
-                    dtprint(prompt)
-
-                except FileNotFoundError:
-                    comm_utils.send_text(client_socket, "FILEError")
-                    raise FileNotFoundError(f"Error: '{os.path.basename(full_path)}' not found")
-                except OSError as oe:
-                    comm_utils.send_text(client_socket, "OSError")
-                    raise OSError(f"Error: OS related error. Maybe your path wasn't a directory?")
-            case "/exit":
-                if comm_utils.receive_text(client_socket) == "y":
-                    dtprint(f"Client {client_address} closed the connection with the server.")
-                    break
-            case "/upload":
-                if sv_utils.confirm_filetype_with_client(client_socket, full_path):
-                    dtprint(f"Receiving a file from the client {client_address}.")
-                    comm_utils.receive_file(client_socket, arg1)
-            case "/download":
-                if sv_utils.confirm_filetype_with_client(client_socket, full_path):
-                    dtprint(f"Sending a file to the client {client_address}.")
-                    comm_utils.send_file(client_socket, full_path)
-            case "/delete":
-                if sv_utils.confirm_filetype_with_client(client_socket, full_path):
-                    f_utils.delete_file(full_path)
-                    comm_utils.send_text(client_socket, f"'{arg1}' deleted successfully.")
-                    dtprint(
-                        f"Deleted the file '{arg1}'. (Requested from client {client_address})"
-                    )
-            case "missarg":
-                dtprint(f"Client {client_address} action was cancelled due to missing arguments.")
-
-    except FileNotFoundError as fe:
-        dtprint(f"File error. Sending to client the report:\n{' '*TIMESTMP_OFFSET}'{fe}'")
-        comm_utils.send_text(client_socket, f"{fe}")
-    except PermissionError as pe:
-        dtprint(f"Client error. Sending to client the report:\n{' '*TIMESTMP_OFFSET}'{pe}'")
-        comm_utils.send_text(client_socket, f"{pe}")
-    except BrokenPipeError as be:
-        dtprint(
-            f"Client error: The client {client_address} has closed the connection unexpectedly."
+        # Create a new thread to handle the client
+        client_thread = threading.Thread(
+            target=sv_utils.handle_client, args=(client_socket, client_address)
         )
-    except OSError as oe:
-        dtprint(f"OS related error. Sending to client the report:\n{' '*TIMESTMP_OFFSET}'{oe}'")
-        comm_utils.send_text(client_socket, f"{oe}")
+        client_thread.start()
+        threads.append(client_thread)
 
+        # Remove finished threads
+        threads = [thread for thread in threads if thread.is_alive()]
+except KeyboardInterrupt:
+    print()
+    dtprint(
+        "Server shutting down sequence started. Waiting for active clients to finish."
+    )
+    for thread in threads:
+        thread.join()
 
-# Close the connection
-client_socket.close()
+dtprint("All clients have finished. Shutting down the server now.")
 server_socket.close()
--- server_utils.py
+++ server_utils.py
@@ -2,10 +2,12 @@ import os
 import socket
 
 import communication_utils as comm_utils
+import file_utils as f_utils
+
 dtprint = comm_utils.timestamped_print
 
-CHUNK_SIZE = 1024
-TIMESTMP_OFFSET = 22
+TIMESTMP_OFFSET = " " * 24
+HOME_DIR = os.getcwd()
 
 
 def receive_request(client_socket: socket.socket) -> list:
@@ -14,7 +16,9 @@ def receive_request(client_socket: socket.socket) -> list:
     return request.split()
 
 
-def confirm_filetype_with_client(client_socket: socket.socket, file_path: str) -> bool:
+def confirm_filetype_with_client(
+    client_socket: socket.socket, client_address: tuple, file_path: str
+) -> bool:
     """
     Sends to the client which type of file they're requesting and waits for the validation from the
     client side to continue with the operation.
@@ -35,5 +39,112 @@ def confirm_filetype_with_client(client_socket: socket.socket, file_path: str) -
     if client_feedback == "error":
         wrong_file_requested = comm_utils.receive_text(client_socket)
         fe = f"Error: '{wrong_file_requested}' not found"
-        dtprint(f"File error. Sending to client the report:\n{' '*TIMESTMP_OFFSET}'{fe}'")
+        dtprint(
+            f"File error. Sending the report to client {client_address}:\n{TIMESTMP_OFFSET}'{fe}'"
+        )
     return client_feedback == "y"
+
+
+def handle_client(client_socket: socket.socket, client_address: tuple):
+    """
+    Function to handle each client connection in a separate thread.
+    """
+    dtprint(f"Connection from {client_address} accepted.")
+
+    # Run the server requests until client exits.
+    while True:
+        requests = receive_request(client_socket)
+        request_type, arg1, *_ = requests + [None]
+        dtprint(f"Client {client_address} requested '{request_type[1:]}' command.")
+
+        try:
+            if arg1:
+                full_path = os.path.abspath(os.path.join(os.getcwd(), arg1))
+
+                if not full_path.startswith(HOME_DIR):
+                    raise PermissionError(
+                        f"Error: You don't have permission to access that directory"
+                    )
+                comm_utils.send_text(client_socket, "ok")
+            else:
+                full_path = HOME_DIR
+                comm_utils.send_text(client_socket, "ok")
+
+            match request_type:
+                # Both listing functions have to check for error as they don't pass through the
+                # validation function.
+                case "/list" | "/ls" | "/tree":
+                    try:
+                        listing_function = {
+                            "/ls": f_utils.list_content,
+                            "/list": f_utils.list_content,
+                            "/tree": f_utils.tree_list_content,
+                        }
+                        dir_list = listing_function[request_type](full_path)
+
+                        comm_utils.send_text(client_socket, "ok")
+                        comm_utils.send_text(client_socket, dir_list)
+
+                        prompt = f"Listed a directory for client {client_address}"
+                        prompt += " as a tree." if request_type == "/tree" else "."
+                        dtprint(prompt)
+
+                    except FileNotFoundError:
+                        comm_utils.send_text(client_socket, "FILEError")
+                        raise FileNotFoundError(
+                            f"Error: '{os.path.basename(full_path)}' not found"
+                        )
+                    except OSError as oe:
+                        comm_utils.send_text(client_socket, "OSError")
+                        raise OSError(
+                            f"Error: OS related error. Maybe your path wasn't a directory?"
+                        )
+                case "/exit":
+                    if comm_utils.receive_text(client_socket) == "y":
+                        dtprint(f"Client {client_address} closed the connection with the server.")
+                        break
+                case "/upload":
+                    if confirm_filetype_with_client(client_socket, client_address, full_path):
+                        dtprint(f"Receiving a file from the client {client_address}.")
+                        comm_utils.receive_file(client_socket, arg1)
+                        print(f"{TIMESTMP_OFFSET}(From client {client_address}.")
+                case "/download":
+                    if confirm_filetype_with_client(client_socket, client_address, full_path):
+                        dtprint(f"Sending a file to the client {client_address}.")
+                        comm_utils.send_file(client_socket, full_path)
+                        print(f"{TIMESTMP_OFFSET}(From client {client_address}.")
+                case "/delete":
+                    if confirm_filetype_with_client(client_socket, client_address, full_path):
+                        f_utils.delete_file(full_path)
+                        comm_utils.send_text(client_socket, f"'{arg1}' deleted successfully.")
+                        dtprint(
+                            f"Deleted the file '{arg1}'. (Requested from client {client_address})"
+                        )
+                case "missarg":
+                    dtprint(
+                        f"Client {client_address} action was cancelled due to missing arguments."
+                    )
+
+        except FileNotFoundError as fe:
+            dtprint(
+                f"File error. Sending the report to client {client_address}:\n{TIMESTMP_OFFSET}'{fe}'"
+            )
+            comm_utils.send_text(client_socket, f"{fe}")
+        except PermissionError as pe:
+            dtprint(
+                f"Client error. Sending the report to client {client_address}:\n{TIMESTMP_OFFSET}'{pe}'"
+            )
+            comm_utils.send_text(client_socket, f"{pe}")
+        except BrokenPipeError:
+            dtprint(
+                f"Client error: The client {client_address} has closed the connection unexpectedly."
+            )
+        except OSError as oe:
+            dtprint(
+                f"OS related error. Sending the report to client {client_address}:\n{TIMESTMP_OFFSET}'{oe}'"
+            )
+            comm_utils.send_text(client_socket, f"{oe}")
+
+    # Close the connection for this client
+    client_socket.close()
+    dtprint(f"Connection with {client_address} closed.")
